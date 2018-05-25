23533e43ff104d29f23ed98fae0a37a39ad5c53b
Atsushi Iwasa <a-iwasa@m3.com>
2018-05-25 21:14:29 +0900

    initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,148 @@
+import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
+
+object Main extends App {
+  import Stream._
+
+  val a = Stream.apply(1, 2, 3)
+  println(a.toList)
+
+  var b = ArrayBuffer.empty[Int]
+  for(x <- List(1, 2, 3, 4)) {
+    val y = x + 10
+    if(y % 2 == 0) {
+      b.append(y * 3)
+    }
+  }
+
+  for {
+    x <- List(1, 2, 3, 4)
+    y = x + 10
+    if y % 2 == 0
+  } yield
+    y * 3
+
+  List(1, 2, 3, 4).collect {
+    case x if (x + 10) % 2 == 0 => (x + 10) * 3
+  }
+
+  println(ones.take(5).toList)
+  println(ones.exists(_ % 2 != 0))
+  println(ones.map(_ + 1).exists(_ % 2 == 0))
+  println(ones.takeWhile(_ == 1))
+  println(ones.forAll(_ != 1))
+
+  println(unfold(1)(s => Some((s + 2, s + 2))).take(5).toList)
+}
+
+trait Stream[+A] {
+  import Stream._
+
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+
+  def toList: List[A] = this match {
+    case Cons(a, tail) => a() +: tail().toList
+    case Empty => Nil
+  }
+
+  def toListTailrec: List[A] = {
+    @tailrec
+    def go(s: Stream[A], l: List[A]): List[A] = s match {
+      case Cons(a, tail) => go(tail(), a() +: l)
+      case Empty => l
+    }
+
+    go(this, Nil).reverse
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Empty => Empty
+    case _ if n <= 0 => Empty
+    case Cons(a, tail) => cons[A](a(), tail().take(n - 1))
+  }
+
+  @tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(a, tail) if n > 0 => tail().drop(n - 1)
+    case _ => this
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(a, tail) if p(a()) => cons(a(), tail().takeWhile(p))
+    case _ => Empty
+  }
+
+  def exists(p: A => Boolean): Boolean = this match {
+    case Cons(h, t) => p(h()) || t().exists(p)
+    case _ => false
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def exists2(p: A => Boolean): Boolean = foldRight(false)((a, b) => p(a) || b)
+
+  def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
+
+  def takeWhile2(p: A => Boolean): Stream[A] = foldRight[Stream[A]](Empty)(
+    (a, b) => if(p(a)) cons(a, b) else Empty
+  )
+
+  def headOption2: Option[A] = foldRight[Option[A]](None)(
+    (a, b) => Some(a)
+  )
+
+  def map[B](f: A => B): Stream[B] = foldRight[Stream[B]](Empty)((a, b) =>
+    cons(f(a), b)
+  )
+
+  def filter(p: A => Boolean): Stream[A] = foldRight[Stream[A]](Empty)((a, b) =>
+    if(p(a)) cons(a, b) else b
+  )
+
+  def append[B >: A](other: Stream[B]): Stream[B] = foldRight[Stream[B]](other)((a, b) =>
+    cons(a, b)
+  )
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight[Stream[B]](Empty)((a, b) =>
+    f(a).append(b)
+  )
+
+  def find(p: A => Boolean): Option[A] = filter(p).headOption
+
+
+}
+
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+
+  val ones: Stream[Int] = Stream.cons(1, ones)
+
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+  def fibs(m: Int = 0, n: Int = 1): Stream[Int] = cons(m, fibs(n, m + n))
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case None => Empty
+    case Some((a, s)) => cons(a, unfold(s)(f))
+  }
+}
---
+++ src/test/scala/MainSpecification.scala
@@ -0,0 +1,119 @@
+import org.scalacheck.{Arbitrary, Gen, Properties}
+import org.scalacheck.Prop._
+
+object MainSpecification extends Properties("Stream") {
+  import Stream._
+
+  implicit def arbStream[T](implicit a: Arbitrary[T]): Arbitrary[Stream[T]] = Arbitrary {
+    Arbitrary.arbitrary[List[T]].map(ls => Stream.apply(ls: _*))
+  }
+
+  property("toList") = forAll { l: List[Int] =>
+    Stream.apply(l:_*).toList == l
+  }
+
+  property("headOption") = forAll { l: Stream[Int] =>
+    l.headOption == l.toList.headOption
+  }
+
+  property("toListTailrec") = forAll { l: List[Int] =>
+    Stream.apply(l:_*).toListTailrec == l
+  }
+
+  property("take") = forAll { (l: Stream[Int], i: Int) =>
+    l.take(i).toList == l.toList.take(i)
+  }
+
+  property("drop") = forAll { (l: Stream[Int], i: Int) =>
+    l.drop(i).toList == l.toList.drop(i)
+  }
+
+  def createP(i: Int) = {
+    val p = (x: Int) => x > i
+    val q = (x: Int) => x < i
+    val r = (x: Int) => if(i != 0) x % i == 0 else false
+    Seq(p, q, r)
+  }
+
+  property("takeWhile") = forAll { (l: Stream[Int], i: Int) =>
+    createP(i).forall(p => l.takeWhile(p).toList == l.toList.takeWhile(p))
+  }
+
+  property("exists") = forAll { (l: Stream[Int], i: Int) =>
+    createP(i).forall(p => l.exists(p) == l.toList.exists(p))
+  }
+
+  property("foldRight") = forAll { (l: Stream[Int], i: Int) =>
+    l.foldRight(i)((a, b) => a + b) == l.toList.foldRight(i)((a, b) => a + b)
+    l.foldRight(i)((a, b) => a * b) == l.toList.foldRight(i)((a, b) => a * b)
+  }
+
+  property("exists2") = forAll { (l: Stream[Int], i: Int) =>
+    createP(i).forall(p => l.exists2(p) == l.toList.exists(p))
+  }
+
+  property("forAll") = forAll { (l: Stream[Int], i: Int) =>
+    createP(i).forall(p => l.forAll(p) == l.toList.forall(p))
+  }
+
+  property("takeWhile2") = forAll { (l: Stream[Int], i: Int) =>
+    createP(i).forall(p => l.takeWhile2(p).toList == l.toList.takeWhile(p))
+  }
+
+  property("headOption2") = forAll { l: Stream[Int] =>
+    l.headOption2 == l.toList.headOption
+  }
+
+  property("map") = forAll { (l: Stream[Int], i: Int) =>
+    createP(i).forall(p => l.map(p).toList == l.toList.map(p))
+  }
+
+  property("filter") = forAll { (l: Stream[Int], i: Int) =>
+    createP(i).forall(p => l.filter(p).toList == l.toList.filter(p))
+  }
+
+  property("append") = forAll { (l: Stream[Int], m: Stream[Int]) =>
+    l.append(m).toList == l.toList ++ m.toList
+  }
+
+  property("flatMap") = forAll { (l: Stream[Int], a: Int, b: Int) =>
+    val f = (x: Int) => Stream.apply(x, x * a, x * b)
+    l.flatMap(f).toList == l.toList.flatMap(f andThen (_.toList))
+  }
+
+  property("find") = forAll { (l: Stream[Int], i: Int) =>
+    createP(i).forall(p => l.find(p) == l.toList.find(p))
+  }
+
+  type SmallInt = Int
+  implicit val smallIntArbitrary: Arbitrary[SmallInt] = Arbitrary(Gen.choose(0, 100))
+
+  property("constant") = forAll { (a: Int, b: SmallInt) =>
+    constant(a).take(b).forAll(_ == a)
+  }
+
+  property("from") = forAll { (a: Int, b: SmallInt) =>
+    from(a).take(b).toList == Range(a, a + b).toList
+  }
+
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, a: Int, b: Int): Int =
+      if (n == 0) a
+      else go(n - 1, b, a + b)
+
+    go(n, 0, 1)
+  }
+
+  property("fibs") = forAll { n: SmallInt =>
+    fibs().drop(n).headOption.get == fib(n)
+  }
+
+  property("unfold") = forAll { (a: SmallInt, b: SmallInt, n: SmallInt) =>
+    unfold(a)(s => Some((s, s + b))).drop(n).headOption.get == a + b * n
+    ones.take(n).toList == unfold(a)(_ => Some((1, a))).take(n).toList
+    constant(a).take(n).toList == unfold(b)(_ => Some(a, b)).take(n).toList
+    from(a).take(n).toList == unfold(a)(x => Some((x, x + 1))).take(n).toList
+    fibs().take(n).toList == unfold((0, 1))({ case (x, y) => Some(x, (y, x + y)) }).take(n).toList
+  }
+}
